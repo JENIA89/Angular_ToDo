61a62b2702ebbe3155bfe55a1cf19f2426535d96
Evgene Popolamov <popolamov908@gmail.com>
2021-05-11 22:02:44 +0300

    feat: add form functions

---
+++ src/app/shared/data.service.ts
@@ -0,0 +1,33 @@
+import { Todo } from './todo.module';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  todos: Todo[]=[
+    new Todo('This is a test', true),
+    new Todo('Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis fugit magni facilis animi, deserunt modi,')
+  ]
+
+  constructor() { }
+
+  addTodo(todo: Todo){
+    this.todos.push(todo)
+  }
+
+  getAllTodo(){
+    return this.todos
+  }
+
+  updateTodo(index: number, updateTodo: Todo){
+     this.todos[index] = updateTodo
+
+  }   
+
+  deleteTodo(index: number){
+    this.todos.splice(index, 1)
+  }
+
+  }
--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,4 +1,7 @@
+import { DataService } from './../shared/data.service';
+import { Todo } from './../shared/todo.module';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-todos',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodosComponent implements OnInit {
 
-  constructor() { }
+  todos: Todo[]
+  showValidErrors: boolean
+
+  constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
+    this.todos = this.dataService.getAllTodo()
+  }
+
+  onFormSubmit(form: NgForm){
+    if(form.invalid) return this.showValidErrors=true
+    this.dataService.addTodo(new Todo(form.value.text))
+    this.showValidErrors=false
+    form.reset()
   }
 
 }
+ 
